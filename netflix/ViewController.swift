f9e359ba5aecc80cf5cdd5c0ede6ed15b3336fbd
jimmy <wjdtjq6@gmail.com>
2024-05-16 15:08:01 +0900

    Initial Commit

---
+++ netflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netflix
+//
+//  Created by t2023-m0032 on 5/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
